c9975546baa3a7805338eccc49da6780645b0569
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-07-19 21:57:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+
+const selector = document.getElementById("colorSelect");
+const button = document.querySelector("input");
+
+ button.addEventListener("click", listener);
+
+function listener() 
+	{
+		for(let i=0;i<selector.children.length;i++)
+			{
+				if(selector.children[i].value === selector.value)
+				{
+					selector.children[i].remove();
+				}
+			}
+	}
+
+
+
+
+
+
+
+
+
